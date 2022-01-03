3fe185f43516c2ef629d68e223c6567017c344a5
paulkoeckdev <paul.koeck@gmx.net>
2022-01-03 23:35:53 +0100

    Added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "Remark API",
+      cwd: "./",
+      script: "./apps/api/dist/main.js",
+      exec_mode: "cluster",
+      instances: "max",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "Remark CDN",
+      cwd: "./",
+      script: "./apps/cdn/dist/main.js",
+      exec_mode: "cluster",
+      instances: "max",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "Remark Web",
+      cwd: "./apps/web/dist",
+      script: "next start",
+    },
+  ],
+};
