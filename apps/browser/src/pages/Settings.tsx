459f734b506ce631932a1f3c7fa8be75aff28ce9
Paul Koeck <paul@koeck.dev>
2022-01-22 15:07:34 +0100

    Fixed button

--- apps/browser/src/pages/Settings.tsx
+++ apps/browser/src/pages/Settings.tsx
@@ -32,7 +32,7 @@ export default function Settings(props: ISettingsProps) {
   }
 
   function clearHide() {
-    if (clearedSmart) return;
+    if (clearedHide) return;
     setClearedHide(true);
   }
 
