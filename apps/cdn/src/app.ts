c1098bd132b78bdd2f31be3a824e24ece4e4b7b0
Paul Köck <paul@koeck.dev>
2024-03-12 17:56:58 +0100

    Remove wss project

--- apps/cdn/src/app.ts
+++ apps/cdn/src/app.ts
@@ -9,14 +9,14 @@ import env from "@util/env";
 
 const app = express();
 
-app.disable("etag");
+app.disable("etag")
 app.disable("x-powered-by");
 
 app.set("trust proxy", true);
 
 app.use(express.json());
 
-app.use("/avatar", avatar);
+app.use("/avatar", avatar)
 app.use("/og", og);
 
 app.listen(env("PORT"), () => {
